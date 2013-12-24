ce28c5459d656863a290a3a442d2e6de11be2d61
paulgrimes <paulg@arkade.com.au>
2013-12-24 17:53:37 +1100

    Pointing the demo namespace.

--- simple_test.php
+++ simple_test.php
@@ -5,7 +5,7 @@ include_once("gamifyws.php");
 
 use gamifyws\gamifyws;
 
-$gamify = new gamifyws('testing', 'e2156f086656426f2b6cc1ff8909d534', '7146df91deaddbcb581d26daa29b95b3');
+$gamify = new gamifyws('demo', 'd7fa79e4b54614bd4befcb25edf07d3d', '641dc005bb8c0333b8ecabde819bbe6b');
 
 $details = $gamify->create_action('Someone Does Something');
 echo $details;
